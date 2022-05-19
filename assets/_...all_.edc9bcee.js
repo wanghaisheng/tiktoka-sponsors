782cb3bfbd97340d9f8f1bd085bfc9f90c205784
wanghaisheng <wanghaisheng@users.noreply.github.com>
2022-05-19 04:47:59 +0000

    deploy: 777052a4783740e798546953aeb5f88b21728f66

---
+++ assets/_...all_.edc9bcee.js
@@ -0,0 +1,1 @@
+import{d as o,u as e,o as a,c as s,t as c,a as r}from"./app.acb1f828.js";var t={};const u=o({setup(f){const{t:n}=e();return(p,i)=>(a(),s("div",null,c(r(n)("not-found")),1))}});typeof t=="function"&&t(u);export{u as default};
